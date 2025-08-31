7f668661197d4ff685cc8665b45aa38871caaeaf
sandeepkr2607 <sandeepkr2607@gmail.com>
2025-08-31 17:36:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Layout from './Layout';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
